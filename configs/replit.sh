bc5a12fd1625c3466b9bcc52a24c5d0ad953d7f5
programmeruser2 <69394951+programmeruser2@users.noreply.github.com>
2021-03-22 19:17:23 -0400

    Start rewrite with Ubuntu Base

--- configs/replit.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cat ~/.config/chromium/Local\ State | perl -pe "s/\"bottom.*/\"bottom\": $(xrandr | grep \* | cut -d' ' -f4 | cut -d'x' -f2),/" > ~/.config/chromium/Local\ State
-cat ~/.config/chromium/Local\ State | perl -pe "s/\"right.*/\"right\": $(xrandr | grep \* | cut -d' ' -f4 | cut -d'x' -f1),/" > ~/.config/chromium/Local\ State
-while true; do chromium-browser https://repl.it --kiosk --start-maximized; done
